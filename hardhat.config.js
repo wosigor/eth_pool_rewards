0773e9b0fd9e614c6b15acbe3e62b153659af0e6
IW <wosigor@gmail.com>
2022-09-19 20:40:39 +0200

    Add task to check balance

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,14 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.utils.formatEther(balance), "ETH");
+  });
+
 module.exports = {
   defaultNetwork: "matic",
   networks: {
